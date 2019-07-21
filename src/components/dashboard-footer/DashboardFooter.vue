a0f7ce9d71463f0615c19b2dacdf11a36a68451f
Jared Booker <jbooker@digitalmint.io>
2019-07-20 19:36:25 -0500

    updated

--- src/components/dashboard-footer/DashboardFooter.vue
+++ src/components/dashboard-footer/DashboardFooter.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="footer">
-    <small class="p-footer">built and designed by Jared Booker &copy; {{ new Date().getFullYear() }}</small>
+    <small>built and designed by Jared Booker &copy; {{ new Date().getFullYear() }}</small>
+    <br>
+    <small>
+      <a href="https://iexcloud.io" target="_blank">Data provided by IEX Cloud</a>
+    </small>
   </div>
 </template>
 <script>
@@ -10,18 +14,30 @@ export default {
 </script>
 <style>
 .footer {
-  margin-top: 2em;
   bottom: 0;
   width: 100%;
-  display: flex;
+  margin: auto;
+  display: block;
+  /* display: flex; */
+  text-align: center;
   border-top: 1px solid rgba(25, 29, 45, 0.85);
   justify-content: center;
   align-content: center;
-  padding: 2em;
+  padding-top: 2em;
+}
+
+.footer a {
+  color: white;
+  text-transform: none;
+  text-decoration: none;
+}
+.footer a:hover {
+  color: rgb(113, 75, 253);
 }
 
 .p-footer {
   display: flex;
+  flex-direction: columns;
   /* text-align:center; */
   align-items: center;
   padding: 0 0.5em;
